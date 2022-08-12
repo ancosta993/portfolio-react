d75528a7f98fbedd9c8432bad91f78d77708df0a
Anthony Dcosta <adcosta2000@gmail.com>
2022-08-11 22:32:09 -0400

    form state set up

--- src/components/pages/Contacts.js
+++ src/components/pages/Contacts.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
 function Contacts(){
+   const [contacts, setContacts] = useState({name:'', email:'', message:''});
+   const {name, email,message} = contacts;
+   const [errorMessage, setErrorMessage] = useState('');
+
+   const handleChange = (e) => {
+      if (!errorMessage) {
+         setContacts({...contacts, [e.target.name]: e.target.value});
+      }
+   };
+   
+
+
    return(
-      <h1>Contact Form</h1>
+      <h1>Contact</h1>
    )
 }
 
